43da2963ae0fb15ba550ad1e131354e2ba6dbd24
leticiamazzoportela <leticiaivpportela@hotmail.com>
2018-03-22 22:54:50 -0300

    funções graph

--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@
 	Graph Class
 '''
 
+import edge
 
 class Graph():
 	"Class to store a edges's array and a vertex's dictionary"
@@ -9,6 +10,15 @@ class Graph():
 	def __init__(self):
 		self.__edges = {} # dicionario de tuplas.. (no source, no dest)
 		self.__vertex = {}
+	
+	def add_edge(self, edge):
+		self.__edges[(edge.get_source(), edge.get_destination())] = edge
+
+	def remove_edge(self, edge):
+		self.__edges.pop((edge.get_source(), edge.get_destination()))
+
+	def add_vertex(self, vertex):
+		self.__vertex[vertex] = []
 
 	def get_edge_from_souce_destination(self, source, destination):
 		"Get a edge from a source and a destination node"
