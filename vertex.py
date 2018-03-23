93abcee88c00f160b466b0573d73e61a95f32fb2
claudiaps <clp.sampedro@gmail.com>
2018-03-22 22:19:52 -0300

    Set and get in vertex class

--- vertex.py
+++ vertex.py
@@ -19,7 +19,17 @@ class Vertex():
 
 		self.__name = name
 
+	def get_name(self, name):
+		"Get a vertex name"
+
+		resturn self.__name
+
 	def set_value(self, value):
 		"Set a vertex value"
 		
 		self.__value = value
+
+	def get_value(self, value):
+		"Get a vertex value"
+
+		resturn self.__value
